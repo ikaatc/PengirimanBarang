cda14c999df67c9feccc7b34ec1788ceaea4fee0
Irawan rofi hidayat <irawan.rofi.ft21@mail.umy.ac.id>
2023-07-06 20:47:04 +0700

    Membuat Form Barang

--- PengirimanBarang/barang.Designer.cs
+++ PengirimanBarang/barang.Designer.cs
@@ -29,12 +29,195 @@ namespace PengirimanBarang
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.btnback = new System.Windows.Forms.Button();
+            this.btnclear = new System.Windows.Forms.Button();
+            this.btnsave = new System.Windows.Forms.Button();
+            this.btnadd = new System.Windows.Forms.Button();
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.nmbarang = new System.Windows.Forms.Label();
+            this.kategoribarang = new System.Windows.Forms.Label();
+            this.jnsbarang = new System.Windows.Forms.Label();
+            this.txtnmbarang = new System.Windows.Forms.TextBox();
+            this.txtjnsbarang = new System.Windows.Forms.TextBox();
+            this.txtkategoribarang = new System.Windows.Forms.TextBox();
+            this.databarang = new System.Windows.Forms.Label();
+            this.tableLayoutPanel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // btnback
+            // 
+            this.btnback.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnback.Location = new System.Drawing.Point(67, 25);
+            this.btnback.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.btnback.Name = "btnback";
+            this.btnback.Size = new System.Drawing.Size(105, 34);
+            this.btnback.TabIndex = 14;
+            this.btnback.Text = "Back";
+            this.btnback.UseVisualStyleBackColor = true;
+            // 
+            // btnclear
+            // 
+            this.btnclear.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnclear.Location = new System.Drawing.Point(528, 306);
+            this.btnclear.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.btnclear.Name = "btnclear";
+            this.btnclear.Size = new System.Drawing.Size(105, 34);
+            this.btnclear.TabIndex = 13;
+            this.btnclear.Text = "Clear";
+            this.btnclear.UseVisualStyleBackColor = true;
+            // 
+            // btnsave
+            // 
+            this.btnsave.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnsave.Location = new System.Drawing.Point(349, 306);
+            this.btnsave.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.btnsave.Name = "btnsave";
+            this.btnsave.Size = new System.Drawing.Size(105, 34);
+            this.btnsave.TabIndex = 12;
+            this.btnsave.Text = "Save";
+            this.btnsave.UseVisualStyleBackColor = true;
+            // 
+            // btnadd
+            // 
+            this.btnadd.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnadd.Location = new System.Drawing.Point(159, 306);
+            this.btnadd.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.btnadd.Name = "btnadd";
+            this.btnadd.Size = new System.Drawing.Size(105, 34);
+            this.btnadd.TabIndex = 11;
+            this.btnadd.Text = "Add";
+            this.btnadd.UseVisualStyleBackColor = true;
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.ColumnCount = 2;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.Controls.Add(this.nmbarang, 0, 0);
+            this.tableLayoutPanel1.Controls.Add(this.kategoribarang, 0, 2);
+            this.tableLayoutPanel1.Controls.Add(this.jnsbarang, 0, 1);
+            this.tableLayoutPanel1.Controls.Add(this.txtnmbarang, 1, 0);
+            this.tableLayoutPanel1.Controls.Add(this.txtjnsbarang, 1, 1);
+            this.tableLayoutPanel1.Controls.Add(this.txtkategoribarang, 1, 2);
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(148, 101);
+            this.tableLayoutPanel1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 3;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 58F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(498, 178);
+            this.tableLayoutPanel1.TabIndex = 10;
+            // 
+            // nmbarang
+            // 
+            this.nmbarang.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right)));
+            this.nmbarang.AutoSize = true;
+            this.nmbarang.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.nmbarang.Location = new System.Drawing.Point(3, 18);
+            this.nmbarang.Name = "nmbarang";
+            this.nmbarang.Size = new System.Drawing.Size(243, 24);
+            this.nmbarang.TabIndex = 0;
+            this.nmbarang.Text = "Nama Barang";
+            this.nmbarang.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // kategoribarang
+            // 
+            this.kategoribarang.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right)));
+            this.kategoribarang.AutoSize = true;
+            this.kategoribarang.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.kategoribarang.Location = new System.Drawing.Point(3, 137);
+            this.kategoribarang.Name = "kategoribarang";
+            this.kategoribarang.Size = new System.Drawing.Size(243, 24);
+            this.kategoribarang.TabIndex = 2;
+            this.kategoribarang.Text = "Kategori Barang";
+            this.kategoribarang.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // jnsbarang
+            // 
+            this.jnsbarang.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right)));
+            this.jnsbarang.AutoSize = true;
+            this.jnsbarang.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.jnsbarang.Location = new System.Drawing.Point(3, 78);
+            this.jnsbarang.Name = "jnsbarang";
+            this.jnsbarang.Size = new System.Drawing.Size(243, 24);
+            this.jnsbarang.TabIndex = 1;
+            this.jnsbarang.Text = "Jenis Barang";
+            this.jnsbarang.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // txtnmbarang
+            // 
+            this.txtnmbarang.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtnmbarang.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtnmbarang.Location = new System.Drawing.Point(252, 15);
+            this.txtnmbarang.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.txtnmbarang.Name = "txtnmbarang";
+            this.txtnmbarang.Size = new System.Drawing.Size(243, 30);
+            this.txtnmbarang.TabIndex = 3;
+            // 
+            // txtjnsbarang
+            // 
+            this.txtjnsbarang.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtjnsbarang.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtjnsbarang.Location = new System.Drawing.Point(252, 75);
+            this.txtjnsbarang.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.txtjnsbarang.Name = "txtjnsbarang";
+            this.txtjnsbarang.Size = new System.Drawing.Size(243, 30);
+            this.txtjnsbarang.TabIndex = 4;
+            // 
+            // txtkategoribarang
+            // 
+            this.txtkategoribarang.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtkategoribarang.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtkategoribarang.Location = new System.Drawing.Point(252, 134);
+            this.txtkategoribarang.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.txtkategoribarang.Name = "txtkategoribarang";
+            this.txtkategoribarang.Size = new System.Drawing.Size(243, 30);
+            this.txtkategoribarang.TabIndex = 5;
+            // 
+            // databarang
+            // 
+            this.databarang.AutoSize = true;
+            this.databarang.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.databarang.Location = new System.Drawing.Point(309, 57);
+            this.databarang.Name = "databarang";
+            this.databarang.Size = new System.Drawing.Size(154, 29);
+            this.databarang.TabIndex = 9;
+            this.databarang.Text = "Data Barang";
+            // 
+            // barang
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(712, 364);
+            this.Controls.Add(this.btnback);
+            this.Controls.Add(this.btnclear);
+            this.Controls.Add(this.btnsave);
+            this.Controls.Add(this.btnadd);
+            this.Controls.Add(this.tableLayoutPanel1);
+            this.Controls.Add(this.databarang);
+            this.Name = "barang";
             this.Text = "barang";
+            this.tableLayoutPanel1.ResumeLayout(false);
+            this.tableLayoutPanel1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button btnback;
+        private System.Windows.Forms.Button btnclear;
+        private System.Windows.Forms.Button btnsave;
+        private System.Windows.Forms.Button btnadd;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.Label nmbarang;
+        private System.Windows.Forms.Label kategoribarang;
+        private System.Windows.Forms.Label jnsbarang;
+        private System.Windows.Forms.TextBox txtnmbarang;
+        private System.Windows.Forms.TextBox txtjnsbarang;
+        private System.Windows.Forms.TextBox txtkategoribarang;
+        private System.Windows.Forms.Label databarang;
     }
 }
--- PengirimanBarang/barang.cs
+++ PengirimanBarang/barang.cs
@@ -16,5 +16,10 @@ namespace PengirimanBarang
         {
             InitializeComponent();
         }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
     }
 }

3700ee8635bedc55d5f6dbe7419be66303dca84d
Irawan rofi hidayat <irawan.rofi.ft21@mail.umy.ac.id>
2023-07-06 22:19:48 +0700

    Membuat Form Kurir

---
+++ PengirimanBarang/kurir.Designer.cs
@@ -0,0 +1,222 @@
+﻿namespace PengirimanBarang
+{
+    partial class kurir
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnback = new System.Windows.Forms.Button();
+            this.btnclear = new System.Windows.Forms.Button();
+            this.btnsave = new System.Windows.Forms.Button();
+            this.btnadd = new System.Windows.Forms.Button();
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.idkurir = new System.Windows.Forms.Label();
+            this.nokurir = new System.Windows.Forms.Label();
+            this.nmkurir = new System.Windows.Forms.Label();
+            this.txtkurir = new System.Windows.Forms.TextBox();
+            this.txtnmkurir = new System.Windows.Forms.TextBox();
+            this.txtnokurir = new System.Windows.Forms.TextBox();
+            this.datakurir = new System.Windows.Forms.Label();
+            this.tableLayoutPanel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // btnback
+            // 
+            this.btnback.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnback.Location = new System.Drawing.Point(65, 33);
+            this.btnback.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.btnback.Name = "btnback";
+            this.btnback.Size = new System.Drawing.Size(105, 34);
+            this.btnback.TabIndex = 14;
+            this.btnback.Text = "Back";
+            this.btnback.UseVisualStyleBackColor = true;
+            // 
+            // btnclear
+            // 
+            this.btnclear.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnclear.Location = new System.Drawing.Point(526, 314);
+            this.btnclear.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.btnclear.Name = "btnclear";
+            this.btnclear.Size = new System.Drawing.Size(105, 34);
+            this.btnclear.TabIndex = 13;
+            this.btnclear.Text = "Clear";
+            this.btnclear.UseVisualStyleBackColor = true;
+            // 
+            // btnsave
+            // 
+            this.btnsave.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnsave.Location = new System.Drawing.Point(347, 314);
+            this.btnsave.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.btnsave.Name = "btnsave";
+            this.btnsave.Size = new System.Drawing.Size(105, 34);
+            this.btnsave.TabIndex = 12;
+            this.btnsave.Text = "Save";
+            this.btnsave.UseVisualStyleBackColor = true;
+            // 
+            // btnadd
+            // 
+            this.btnadd.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnadd.Location = new System.Drawing.Point(157, 314);
+            this.btnadd.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.btnadd.Name = "btnadd";
+            this.btnadd.Size = new System.Drawing.Size(105, 34);
+            this.btnadd.TabIndex = 11;
+            this.btnadd.Text = "Add";
+            this.btnadd.UseVisualStyleBackColor = true;
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.ColumnCount = 2;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.Controls.Add(this.idkurir, 0, 0);
+            this.tableLayoutPanel1.Controls.Add(this.nokurir, 0, 2);
+            this.tableLayoutPanel1.Controls.Add(this.nmkurir, 0, 1);
+            this.tableLayoutPanel1.Controls.Add(this.txtkurir, 1, 0);
+            this.tableLayoutPanel1.Controls.Add(this.txtnmkurir, 1, 1);
+            this.tableLayoutPanel1.Controls.Add(this.txtnokurir, 1, 2);
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(146, 109);
+            this.tableLayoutPanel1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 3;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 58F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(498, 178);
+            this.tableLayoutPanel1.TabIndex = 10;
+            // 
+            // idkurir
+            // 
+            this.idkurir.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right)));
+            this.idkurir.AutoSize = true;
+            this.idkurir.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.idkurir.Location = new System.Drawing.Point(3, 18);
+            this.idkurir.Name = "idkurir";
+            this.idkurir.Size = new System.Drawing.Size(243, 24);
+            this.idkurir.TabIndex = 0;
+            this.idkurir.Text = "ID Kurir";
+            this.idkurir.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // nokurir
+            // 
+            this.nokurir.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right)));
+            this.nokurir.AutoSize = true;
+            this.nokurir.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.nokurir.Location = new System.Drawing.Point(3, 137);
+            this.nokurir.Name = "nokurir";
+            this.nokurir.Size = new System.Drawing.Size(243, 24);
+            this.nokurir.TabIndex = 2;
+            this.nokurir.Text = "No Telepon Kurir";
+            this.nokurir.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // nmkurir
+            // 
+            this.nmkurir.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right)));
+            this.nmkurir.AutoSize = true;
+            this.nmkurir.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.nmkurir.Location = new System.Drawing.Point(3, 78);
+            this.nmkurir.Name = "nmkurir";
+            this.nmkurir.Size = new System.Drawing.Size(243, 24);
+            this.nmkurir.TabIndex = 1;
+            this.nmkurir.Text = "Nama Kurir";
+            this.nmkurir.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // txtkurir
+            // 
+            this.txtkurir.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtkurir.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtkurir.Location = new System.Drawing.Point(252, 15);
+            this.txtkurir.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.txtkurir.Name = "txtkurir";
+            this.txtkurir.Size = new System.Drawing.Size(243, 30);
+            this.txtkurir.TabIndex = 3;
+            // 
+            // txtnmkurir
+            // 
+            this.txtnmkurir.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtnmkurir.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtnmkurir.Location = new System.Drawing.Point(252, 75);
+            this.txtnmkurir.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.txtnmkurir.Name = "txtnmkurir";
+            this.txtnmkurir.Size = new System.Drawing.Size(243, 30);
+            this.txtnmkurir.TabIndex = 4;
+            // 
+            // txtnokurir
+            // 
+            this.txtnokurir.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtnokurir.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtnokurir.Location = new System.Drawing.Point(252, 134);
+            this.txtnokurir.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.txtnokurir.Name = "txtnokurir";
+            this.txtnokurir.Size = new System.Drawing.Size(243, 30);
+            this.txtnokurir.TabIndex = 5;
+            // 
+            // datakurir
+            // 
+            this.datakurir.AutoSize = true;
+            this.datakurir.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.datakurir.Location = new System.Drawing.Point(307, 65);
+            this.datakurir.Name = "datakurir";
+            this.datakurir.Size = new System.Drawing.Size(126, 29);
+            this.datakurir.TabIndex = 9;
+            this.datakurir.Text = "Data Kurir";
+            // 
+            // kurir
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(708, 381);
+            this.Controls.Add(this.btnback);
+            this.Controls.Add(this.btnclear);
+            this.Controls.Add(this.btnsave);
+            this.Controls.Add(this.btnadd);
+            this.Controls.Add(this.tableLayoutPanel1);
+            this.Controls.Add(this.datakurir);
+            this.Name = "kurir";
+            this.Text = "kurir";
+            this.tableLayoutPanel1.ResumeLayout(false);
+            this.tableLayoutPanel1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnback;
+        private System.Windows.Forms.Button btnclear;
+        private System.Windows.Forms.Button btnsave;
+        private System.Windows.Forms.Button btnadd;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.Label idkurir;
+        private System.Windows.Forms.Label nokurir;
+        private System.Windows.Forms.Label nmkurir;
+        private System.Windows.Forms.TextBox txtkurir;
+        private System.Windows.Forms.TextBox txtnmkurir;
+        private System.Windows.Forms.TextBox txtnokurir;
+        private System.Windows.Forms.Label datakurir;
+    }
+}

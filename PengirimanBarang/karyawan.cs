8951cd59eb83937b32d48cfc55852bf2a53cef28
Irawan rofi hidayat <irawan.rofi.ft21@mail.umy.ac.id>
2023-07-08 19:48:11 +0700

    Menambahkan stringConnection untuk menginisialisasi string koneksi database

--- PengirimanBarang/karyawan.cs
+++ PengirimanBarang/karyawan.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Data.SqlClient;
 using System.Drawing;
 using System.Linq;
 using System.Text;
@@ -12,6 +13,8 @@ namespace PengirimanBarang
 {
     public partial class karyawan : Form
     {
+        private string stringConnection = "data source=DESKTOP-9NQGA7N\\IKATC;" + "database=PengirimanBarang; User ID = sa; Password = 1234";
+        private SqlConnection koneksi;
         public karyawan()
         {
             InitializeComponent();

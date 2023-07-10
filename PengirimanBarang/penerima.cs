de03ec67f2b6f73978f011cdc16985fe227bae5d
TC1085 <ika.tania.ft21@mail.umy.ac.id>
2023-07-10 13:40:24 +0700

    Menambahkan pembaruan tampilan DataGridView dan form setelah menyimpan data pada method btnsave_Click()

--- PengirimanBarang/penerima.cs
+++ PengirimanBarang/penerima.cs
@@ -117,6 +117,8 @@ namespace PengirimanBarang
 
                 koneksi.Close();
                 MessageBox.Show("Data Berhasil Disimpan", "Sukses", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                dataGridView();
+                refreshform();
             }
         }
     }

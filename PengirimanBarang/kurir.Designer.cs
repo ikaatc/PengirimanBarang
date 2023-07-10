d174579cc2bcae25c0c37e80b5fa3d462b920157
Irawan rofi hidayat <irawan.rofi.ft21@mail.umy.ac.id>
2023-07-10 15:03:39 +0700

    Menambahkan stringConnection untuk menginisialisasi string koneksi database

--- PengirimanBarang/kurir.Designer.cs
+++ PengirimanBarang/kurir.Designer.cs
@@ -101,6 +101,7 @@
             this.btnadd.TabIndex = 26;
             this.btnadd.Text = "Add";
             this.btnadd.UseVisualStyleBackColor = true;
+            this.btnadd.Click += new System.EventHandler(this.btnadd_Click);
             // 
             // tableLayoutPanel1
             // 
@@ -132,10 +133,10 @@
             this.almtkurir.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right)));
             this.almtkurir.AutoSize = true;
             this.almtkurir.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.almtkurir.Location = new System.Drawing.Point(4, 99);
+            this.almtkurir.Location = new System.Drawing.Point(4, 101);
             this.almtkurir.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.almtkurir.Name = "almtkurir";
-            this.almtkurir.Size = new System.Drawing.Size(365, 25);
+            this.almtkurir.Size = new System.Drawing.Size(365, 20);
             this.almtkurir.TabIndex = 7;
             this.almtkurir.Text = "Alamat";
             this.almtkurir.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
@@ -145,10 +146,10 @@
             this.idkurir.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right)));
             this.idkurir.AutoSize = true;
             this.idkurir.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.idkurir.Location = new System.Drawing.Point(4, 9);
+            this.idkurir.Location = new System.Drawing.Point(4, 12);
             this.idkurir.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.idkurir.Name = "idkurir";
-            this.idkurir.Size = new System.Drawing.Size(365, 25);
+            this.idkurir.Size = new System.Drawing.Size(365, 20);
             this.idkurir.TabIndex = 0;
             this.idkurir.Text = "ID Kurir";
             this.idkurir.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
@@ -158,10 +159,10 @@
             this.nmkurir.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right)));
             this.nmkurir.AutoSize = true;
             this.nmkurir.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.nmkurir.Location = new System.Drawing.Point(4, 53);
+            this.nmkurir.Location = new System.Drawing.Point(4, 55);
             this.nmkurir.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.nmkurir.Name = "nmkurir";
-            this.nmkurir.Size = new System.Drawing.Size(365, 25);
+            this.nmkurir.Size = new System.Drawing.Size(365, 20);
             this.nmkurir.TabIndex = 1;
             this.nmkurir.Text = "Nama";
             this.nmkurir.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
@@ -170,40 +171,40 @@
             // 
             this.txtkurir.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right)));
             this.txtkurir.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.txtkurir.Location = new System.Drawing.Point(377, 7);
+            this.txtkurir.Location = new System.Drawing.Point(377, 9);
             this.txtkurir.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.txtkurir.Name = "txtkurir";
-            this.txtkurir.Size = new System.Drawing.Size(366, 30);
+            this.txtkurir.Size = new System.Drawing.Size(366, 26);
             this.txtkurir.TabIndex = 3;
             // 
             // txtnmkurir
             // 
             this.txtnmkurir.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right)));
             this.txtnmkurir.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.txtnmkurir.Location = new System.Drawing.Point(377, 50);
+            this.txtnmkurir.Location = new System.Drawing.Point(377, 52);
             this.txtnmkurir.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.txtnmkurir.Name = "txtnmkurir";
-            this.txtnmkurir.Size = new System.Drawing.Size(366, 30);
+            this.txtnmkurir.Size = new System.Drawing.Size(366, 26);
             this.txtnmkurir.TabIndex = 4;
             // 
             // txtalmtkurir
             // 
             this.txtalmtkurir.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right)));
             this.txtalmtkurir.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.txtalmtkurir.Location = new System.Drawing.Point(377, 96);
+            this.txtalmtkurir.Location = new System.Drawing.Point(377, 98);
             this.txtalmtkurir.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.txtalmtkurir.Name = "txtalmtkurir";
-            this.txtalmtkurir.Size = new System.Drawing.Size(366, 30);
+            this.txtalmtkurir.Size = new System.Drawing.Size(366, 26);
             this.txtalmtkurir.TabIndex = 5;
             // 
             // txtnokurir
             // 
             this.txtnokurir.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right)));
             this.txtnokurir.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.txtnokurir.Location = new System.Drawing.Point(377, 142);
+            this.txtnokurir.Location = new System.Drawing.Point(377, 144);
             this.txtnokurir.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.txtnokurir.Name = "txtnokurir";
-            this.txtnokurir.Size = new System.Drawing.Size(366, 30);
+            this.txtnokurir.Size = new System.Drawing.Size(366, 26);
             this.txtnokurir.TabIndex = 6;
             // 
             // nokurir
@@ -211,10 +212,10 @@
             this.nokurir.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right)));
             this.nokurir.AutoSize = true;
             this.nokurir.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.nokurir.Location = new System.Drawing.Point(4, 145);
+            this.nokurir.Location = new System.Drawing.Point(4, 147);
             this.nokurir.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.nokurir.Name = "nokurir";
-            this.nokurir.Size = new System.Drawing.Size(365, 25);
+            this.nokurir.Size = new System.Drawing.Size(365, 20);
             this.nokurir.TabIndex = 2;
             this.nokurir.Text = "No Telepon";
             this.nokurir.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
@@ -227,14 +228,14 @@
             this.datakurir.Location = new System.Drawing.Point(404, 29);
             this.datakurir.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.datakurir.Name = "datakurir";
-            this.datakurir.Size = new System.Drawing.Size(139, 30);
+            this.datakurir.Size = new System.Drawing.Size(120, 26);
             this.datakurir.TabIndex = 24;
             this.datakurir.Text = "Data Kurir";
             this.datakurir.TextAlign = System.Drawing.ContentAlignment.TopCenter;
             // 
             // kurir
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(1067, 562);
             this.Controls.Add(this.dataGridView1);
--- PengirimanBarang/kurir.cs
+++ PengirimanBarang/kurir.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Data.SqlClient;
 using System.Drawing;
 using System.Linq;
 using System.Text;
@@ -12,9 +13,16 @@ namespace PengirimanBarang
 {
     public partial class kurir : Form
     {
+        private string stringConnection = "data source=DESKTOP-9NQGA7N\\IKATC;" + "database=PengirimanBarang; User ID = sa; Password = 1234";
+        private SqlConnection koneksi;
         public kurir()
         {
             InitializeComponent();
+        }
+
+        private void btnadd_Click(object sender, EventArgs e)
+        {
+
         }
     }
 }

15e6cdcceae3728656cba3d75e117a141ff065d8
TC1085 <ika.tania.ft21@mail.umy.ac.id>
2023-07-11 21:08:53 +0700

    adding statement and initialializationin of the stringconnection variable and connection to class detail_penerima

--- PengirimanBarang/detail_penerima.cs
+++ PengirimanBarang/detail_penerima.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Data.SqlClient;
 using System.Drawing;
 using System.Linq;
 using System.Text;
@@ -12,9 +13,12 @@ namespace PengirimanBarang
 {
     public partial class detail_penerima : Form
     {
+        private string stringConnection = "data source=DESKTOP-9NQGA7N\\IKATC;" + "database=PengirimanBarang; User ID = sa; Password = 1234";
+        private SqlConnection koneksi;
         public detail_penerima()
         {
             InitializeComponent();
+            koneksi = new SqlConnection(stringConnection);
         }
     }
 }
